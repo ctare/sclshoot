2dedc8b89d739e5b4672d1925f6872d32080c0c7
ctare <sankaku900622@yahoo.co.jp>
2016-07-13 23:07:39 +0900

    add setter

--- src/shoot/core/Position.scala
+++ src/shoot/core/Position.scala
@@ -3,13 +3,26 @@ package shoot.core
 /**
   * Created by c0115114 on 2016/07/13.
   */
-class Position (xPos: Int, yPos: Int){
+class Position (var xPos: Int, var yPos: Int){
+  def this(position: Position){
+    this(position.x, position.y)
+  }
+
   def x: Int = {
     xPos
   }
 
   def y: Int = {
     yPos
+  }
+
+  def set(xPos: Int, yPos: Int): Unit ={
+    this.xPos = xPos
+    this.yPos = yPos
+  }
+
+  def set(pos: Position): Unit ={
+    this.set(pos.x, pos.y)
   }
 
   override def toString: String = {
